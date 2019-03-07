0dc39b9a03796a0e40777874f604f3c29ced9883
kylelambert <elyktrebmal@gmail.com>
2019-03-07 16:59:38 -0500

    fixed probs

--- 03model/Main.js
+++ 03model/Main.js
@@ -1,6 +1,8 @@
 // Kyle Lambert - lambert.448
 
-var numGenerations = 5;
+var twigLength = 6;
+
+var numGenerations = 6;
 var rotationAngle = 22.5;
 var productionString = "X";
 var rules = ["X=F-[[X]+X]+F[+FX]-X", "F=FF"];
@@ -9,7 +11,7 @@ var scene = new THREE.Scene();
 scene.background = new THREE.Color( 0x00422b );     // Green surrounding sky
 
 var camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 1000);
-camera.position.set( 0, 0, 400 );
+camera.position.set( 0, 0, (numGenerations * 100)/2.5 );
 
 var renderer = new THREE.WebGLRenderer({antialias: true});
 renderer.setPixelRatio( window.devicePixelRatio );
@@ -33,12 +35,6 @@ function onWindowResize() {
     camera.updateProjectionMatrix();
 }
 
-// Initialize the unit cube
-var cubeGeometry = new THREE.BoxGeometry(0.5, 2, 0.1, 3, 3, 3);
-var cubeMaterial = new THREE.MeshBasicMaterial({color: 0xffffff, depthTest: true});
-var stick = new THREE.Mesh(cubeGeometry, cubeMaterial);
-stick.translateY(-150);
-
 // Create full production string over all generations
 for (var i = 0; i < numGenerations; i++) {
     for (var j = 0; j < rules.length; j++) {
@@ -46,12 +42,19 @@ for (var i = 0; i < numGenerations; i++) {
         var replacement = rules[j].substr(2);
         productionString = productionString.replace(new RegExp(search, 'g'), replacement);
     }
+    twigLength *= 0.7;
 }
 console.log(productionString);
 
+// Initialize the unit cube
+var cubeGeometry = new THREE.BoxGeometry(0.25, twigLength, 0.1, 3, 3, 3);
+var cubeMaterial = new THREE.MeshBasicMaterial({color: 0xffffff, depthTest: true});
+var stick = new THREE.Mesh(cubeGeometry, cubeMaterial);
+stick.translateY(-numGenerations * 18);
+
 var currentRotation = 0;
 var currentPosition = stick.position;
-var previousY = 1;
+var previousY = twigLength/2;
 var previousX = 0;
 
 var positionStack = [];
@@ -63,13 +66,15 @@ for (var i = 0; i < productionString.length; i++) {
     if (productionString.charAt(i) == "F") {
         var newStick = stick.clone();
         newStick.position.set(currentPosition.x, currentPosition.y, currentPosition.z);
-        newStick.translateX(previousX + -1*Math.sin(currentRotation * (3.1415926 / 180)));
-        newStick.translateY(previousY + Math.cos(currentRotation * (3.1415926 / 180)));
+
+        newStick.translateX(previousX + (twigLength/2)*-1*Math.sin(currentRotation * (3.1415926 / 180)));
+        newStick.translateY(previousY + (twigLength/2)*Math.cos(currentRotation * (3.1415926 / 180)));
         newStick.rotateZ(currentRotation * (3.1415926 / 180));
+
         currentPosition = newStick.position;
-        previousY = Math.cos(currentRotation * (3.1415926 / 180));
-        previousX = -1*Math.sin(currentRotation * (3.1415926 / 180));
-        scene.add(newStick)
+        previousY = (twigLength/2)*Math.cos(currentRotation * (3.1415926 / 180));
+        previousX = (twigLength/2)*-1*Math.sin(currentRotation * (3.1415926 / 180));
+        scene.add(newStick);
     } else if (productionString.charAt(i) == "+") {
         currentRotation += rotationAngle;
     } else if (productionString.charAt(i) == "-") {
