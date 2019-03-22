4644955b25adfd1c65cdad836a9b2e83f2e8bebe
kylelambert <elyktrebmal@gmail.com>
2019-03-22 14:00:25 -0400

    Finished project

--- Assignment4/main.js
+++ Assignment4/main.js
@@ -1,4 +1,6 @@
+// Kyle Lambert (lambert.448)
 
+// function to get shaders from other files and begin execution of program
 var init = function() {
     loadTextResource('./vertexShader.glsl', function (vertexError, vertexText) {
 		if (vertexError) {
@@ -23,7 +25,7 @@ var start = function (vertexShaderText, fragmentShaderText) {
 	var gl = canvas.getContext('webgl');
 
 	if (!gl) {
-		alert('Your browser does not support WebGL');
+		alert('WebGL not supported');
 	}
 
 	gl.clearColor(0.27, 0.73, 0.41, 1.0);
@@ -33,9 +35,7 @@ var start = function (vertexShaderText, fragmentShaderText) {
 	gl.frontFace(gl.CCW);
 	gl.cullFace(gl.BACK);
 
-	//
-	// Create shaders
-	// 
+	// Set up vertex and fragment shaders
 	var vertexShader = gl.createShader(gl.VERTEX_SHADER);
 	var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
 
@@ -68,9 +68,7 @@ var start = function (vertexShaderText, fragmentShaderText) {
 		return;
 	}
 
-	//
-	// Create buffer
-	//
+	// Set up vertices and indices for both boxes
 	var boxVertices = 
 	[ // X, Y, Z           U, V
 		// Top
@@ -110,31 +108,85 @@ var start = function (vertexShaderText, fragmentShaderText) {
 		-2.0, -1.0, -1.0,    0, 1,
 	];
 
+	var boxVertices2 = 
+	[ // X, Y, Z           U, V
+		// Top
+		2.0, 1.0, -1.0,   0, 0,
+		2.0, 1.0, 1.0,    0, 1,
+		4.0, 1.0, 1.0,     1, 1,
+		4.0, 1.0, -1.0,    1, 0,
+
+		// Left
+		2.0, 1.0, 1.0,    0, 0,
+		2.0, -1.0, 1.0,   1, 0,
+		2.0, -1.0, -1.0,  1, 1,
+		2.0, 1.0, -1.0,   0, 1,
+
+		// Right
+		4.0, 1.0, 1.0,    1, 1,
+		4.0, -1.0, 1.0,   0, 1,
+		4.0, -1.0, -1.0,  0, 0,
+		4.0, 1.0, -1.0,   1, 0,
+
+		// Front
+		4.0, 1.0, 1.0,    1, 1,
+		4.0, -1.0, 1.0,    1, 0,
+		2.0, -1.0, 1.0,    0, 0,
+		2.0, 1.0, 1.0,    0, 1,
+
+		// Back
+		4.0, 1.0, -1.0,    0, 0,
+		4.0, -1.0, -1.0,    0, 1,
+		2.0, -1.0, -1.0,    1, 1,
+		2.0, 1.0, -1.0,    1, 0,
+
+		// Bottom
+		2.0, -1.0, -1.0,   1, 1,
+		2.0, -1.0, 1.0,    1, 0,
+		4.0, -1.0, 1.0,     0, 0,
+		4.0, -1.0, -1.0,    0, 1,
+	];
+
 	var boxIndices =
 	[
 		// Top
-		0, 1, 2,
-		0, 2, 3,
+		0, 1, 2,	0, 2, 3,
 
 		// Left
-		5, 4, 6,
-		6, 4, 7,
+		5, 4, 6,	6, 4, 7,
 
 		// Right
-		8, 9, 10,
-		8, 10, 11,
+		8, 9, 10,	8, 10, 11,
 
 		// Front
-		13, 12, 14,
-		15, 14, 12,
+		13, 12, 14,	15, 14, 12,
 
 		// Back
-		16, 17, 18,
-		16, 18, 19,
+		16, 17, 18,	16, 18, 19,
 
 		// Bottom
-		21, 20, 22,
-		22, 20, 23
+		21, 20, 22,	22, 20, 23
+	];
+
+	var boxIndices2 =
+	[
+		// Top
+		0, 1, 2,	0, 2, 3,
+
+		// Left
+		5, 4, 6,	6, 4, 7,
+
+		// Right
+		8, 9, 10,	8, 10, 11,
+
+		// Front
+		13, 12, 14,	15, 14, 12,
+
+		// Back
+		16, 17, 18,	16, 18, 19,
+
+		// Bottom
+		21, 20, 22,	22, 20, 23
 	];
 
 	var boxVertexBufferObject = gl.createBuffer();
@@ -145,30 +197,16 @@ var start = function (vertexShaderText, fragmentShaderText) {
 	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, boxIndexBufferObject);
 	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(boxIndices), gl.STATIC_DRAW);
 
-	var positionAttribLocation = gl.getAttribLocation(program, 'vertPosition');
-	var texCoordAttribLocation = gl.getAttribLocation(program, 'vertTexCoord');
-	gl.vertexAttribPointer(
-		positionAttribLocation, // Attribute location
-		3, // Number of elements per attribute
-		gl.FLOAT, // Type of elements
-		gl.FALSE,
-		5 * Float32Array.BYTES_PER_ELEMENT, // Size of an individual vertex
-		0 // Offset from the beginning of a single vertex to this attribute
-	);
-	gl.vertexAttribPointer(
-		texCoordAttribLocation, // Attribute location
-		2, // Number of elements per attribute
-		gl.FLOAT, // Type of elements
-		gl.FALSE,
-		5 * Float32Array.BYTES_PER_ELEMENT, // Size of an individual vertex
-		3 * Float32Array.BYTES_PER_ELEMENT // Offset from the beginning of a single vertex to this attribute
-	);
+	var boxVertexBufferObject2 = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, boxVertexBufferObject2);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(boxVertices2), gl.STATIC_DRAW);
 
-	gl.enableVertexAttribArray(positionAttribLocation);
-	gl.enableVertexAttribArray(texCoordAttribLocation);
+	var boxIndexBufferObject2 = gl.createBuffer();
+	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, boxIndexBufferObject2);
+	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(boxIndices2), gl.STATIC_DRAW);
 
 	//
-	// Create texture
+	// Create all six textures
 	//
 	var boxTexture = gl.createTexture();
 	gl.bindTexture(gl.TEXTURE_2D, boxTexture);
@@ -248,11 +286,12 @@ var start = function (vertexShaderText, fragmentShaderText) {
 	);
 	gl.bindTexture(gl.TEXTURE_2D, null);
 
+	// Put textures in an array
     var textures = [nitroTexture, boxTexture, tntTexture, akuTexture, questionTexture, lifeTexture];
 
-	// Tell OpenGL state machine which program should be active.
 	gl.useProgram(program);
 
+	// Set up world and view matrices
 	var matWorldUniformLocation = gl.getUniformLocation(program, 'mWorld');
 	var matViewUniformLocation = gl.getUniformLocation(program, 'mView');
 	var matProjUniformLocation = gl.getUniformLocation(program, 'mProj');
@@ -271,29 +310,101 @@ var start = function (vertexShaderText, fragmentShaderText) {
 	var xRotationMatrix = new Float32Array(16);
 	var yRotationMatrix = new Float32Array(16);
 
-	//
-	// Main render loop
-	//
+	var xRotationMatrix2 = new Float32Array(16);
+	var yRotationMatrix2 = new Float32Array(16);
+
 	var identityMatrix = new Float32Array(16);
 	mat4.identity(identityMatrix);
-	var angle = 0;
+	var angle = 0;		// for  rotating the boxes
+	var angle2 = 0;
+	var identityMatrix2 = new Float32Array(16);
+	mat4.identity(identityMatrix2);
+
+	// animation loop
 	var loop = function () {
+		
+		var positionAttribLocation = gl.getAttribLocation(program, 'vertPosition');
+		var texCoordAttribLocation = gl.getAttribLocation(program, 'vertTexCoord');
+		gl.vertexAttribPointer(
+			positionAttribLocation, // Attribute location
+			3, // Number of elements per attribute
+			gl.FLOAT, // Type of elements
+			gl.FALSE,
+			5 * Float32Array.BYTES_PER_ELEMENT, // Size of an individual vertex
+			0 // Offset from the beginning of a single vertex to this attribute
+		);
+		gl.vertexAttribPointer(
+			texCoordAttribLocation, // Attribute location
+			2, // Number of elements per attribute
+			gl.FLOAT, // Type of elements
+			gl.FALSE,
+			5 * Float32Array.BYTES_PER_ELEMENT, // Size of an individual vertex
+			3 * Float32Array.BYTES_PER_ELEMENT // Offset from the beginning of a single vertex to this attribute
+		);
+	
+		// bind buffers for first box
+		gl.enableVertexAttribArray(positionAttribLocation);
+		gl.enableVertexAttribArray(texCoordAttribLocation);
+		gl.bindBuffer(gl.ARRAY_BUFFER, boxVertexBufferObject);
+		gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, boxIndexBufferObject);
+		
+		// rotate the first box
 		angle = performance.now() / 1000 / 6 * 2 * Math.PI;
-		mat4.translate(identityMatrix, identityMatrix, [3, 0, 0]);
+		mat4.translate(identityMatrix, identityMatrix, [-3, 0, 0]);
 		mat4.rotate(yRotationMatrix, identityMatrix, angle, [0, 1, 0]);
 		mat4.rotate(xRotationMatrix, identityMatrix, angle / 4, [1, 0, 0]);
 		mat4.mul(worldMatrix, yRotationMatrix, xRotationMatrix);
-		mat4.translate(identityMatrix, identityMatrix, [-3, 0, 0]);
+		mat4.translate(identityMatrix, identityMatrix, [3, 0, 0]);
 		gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, worldMatrix);
 
         gl.clearColor(0.85, 0.6, 0.7, 1.0);
 		gl.clear(gl.DEPTH_BUFFER_BIT | gl.COLOR_BUFFER_BIT);
 		
+		// draw the box with 6 textures
 		for (var i = 0; i < 6; i++) {
 			gl.activeTexture(gl.TEXTURE0);
 			gl.bindTexture(gl.TEXTURE_2D, textures[i]);
 			gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 6*i*2);
 		}
+
+		var positionAttribLocation2 = gl.getAttribLocation(program, 'vertPosition');
+		var texCoordAttribLocation2 = gl.getAttribLocation(program, 'vertTexCoord');
+		gl.vertexAttribPointer(
+			positionAttribLocation2, // Attribute location
+			3, // Number of elements per attribute
+			gl.FLOAT, // Type of elements
+			gl.FALSE,
+			5 * Float32Array.BYTES_PER_ELEMENT, // Size of an individual vertex
+			0 // Offset from the beginning of a single vertex to this attribute
+		);
+		gl.vertexAttribPointer(
+			texCoordAttribLocation2, // Attribute location
+			2, // Number of elements per attribute
+			gl.FLOAT, // Type of elements
+			gl.FALSE,
+			5 * Float32Array.BYTES_PER_ELEMENT, // Size of an individual vertex
+			3 * Float32Array.BYTES_PER_ELEMENT // Offset from the beginning of a single vertex to this attribute
+		);
+	
+		// bind buffers for second box
+		gl.enableVertexAttribArray(positionAttribLocation2);
+		gl.enableVertexAttribArray(texCoordAttribLocation2);
+		gl.bindBuffer(gl.ARRAY_BUFFER, boxVertexBufferObject2);
+		gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, boxIndexBufferObject2);
+	
+		// rotate the single texture box
+		angle2 = performance.now() / 1000 / 6 * 2 * Math.PI;
+		mat4.translate(identityMatrix2, identityMatrix2, [3, 0, 0]);
+		mat4.rotate(yRotationMatrix2, identityMatrix2, angle2, [0, 1, 0]);
+		mat4.rotate(xRotationMatrix2, identityMatrix2, angle2 / 4, [1, 0, 0]);
+		mat4.mul(worldMatrix, yRotationMatrix2, xRotationMatrix2);
+		mat4.translate(identityMatrix2, identityMatrix2, [-3, 0, 0]);
+		gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, worldMatrix);
+
+		// draw the box with one texture
+		gl.activeTexture(gl.TEXTURE0);
+		gl.bindTexture(gl.TEXTURE_2D, textures[1]);
+		gl.drawElements(gl.TRIANGLES, boxIndices2.length, gl.UNSIGNED_SHORT, 0);
 
 		requestAnimationFrame(loop);
 	};
---
+++ Assignment4/readme.txt
@@ -0,0 +1,11 @@
+Kyle Lambert (lambert.448)
+CSE 5542 - Assignment 4 - Textures
+March 22, 2019
+
+I used basic WebGL for this project. Run the 04texture-KyleLambert.html file in a browser to see 
+the program. All of the code is in main.gl. There are also the fragment and vertex shaders included.
+
+To run, you need to host a local server. I used Python 2.7 and "python -m SimpleHTTPServer" command
+in the folder of my project. Then in your browser go to "localhost:8000" and click on the html file
+to see the boxes. This is because the browser blocks external images from the file system due to
+CORS policy.  I included screenshots in the screenshots folder if you can't get it to run. 
